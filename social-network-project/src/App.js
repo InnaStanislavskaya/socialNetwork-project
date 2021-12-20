9e9f579b423477d24a6b1d6d59f6b6e916d070a7
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-12-20 19:45:27 +0200

    connected Router for profile and dialogs

--- social-network-project/src/App.js
+++ social-network-project/src/App.js
@@ -3,19 +3,24 @@ import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
 import Profile from "./components/Profile/Profile"
 import Dialogs from "./components/Dialogs/Dialogs"
-// import {Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className='container wrapper'>
-        <Header/>
-        <Navbar/>
-        <div className='content'>
-          <Profile/>
+    <BrowserRouter>
+      <div className="App">
+        <div className='container wrapper'>
+          <Header/>
+          <Navbar/>
+          <div className='content'>
+            <Routes>
+              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/dialogs" element={<Dialogs/>}/>
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
