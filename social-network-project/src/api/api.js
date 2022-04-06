b4c579277ca17c1e0d62d67964c4d5da9ad3b788
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2022-04-06 15:12:42 +0300

    DAL, axios.create for getUsers

---
+++ social-network-project/src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "0b242c6f-e752-4033-87fc-0150099d8044"
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 5) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(respons => {
+                return respons.data;
+            })
+    }
+}
