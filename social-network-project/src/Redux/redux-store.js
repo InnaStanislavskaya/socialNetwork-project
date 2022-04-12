cabe74c3884390de725724baa4c9f0863d83d2d7
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2022-04-12 13:54:07 +0300

    LoginForm / reduxform

--- social-network-project/src/Redux/redux-store.js
+++ social-network-project/src/Redux/redux-store.js
@@ -5,7 +5,7 @@ import profileReducer from './profile-reducer';
 import sidebarReducer from './sidebar-reducer';
 import usersReducer from './users-reducer';
 import thunkMiddleware from "redux-thunk";
-
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -13,6 +13,7 @@ let reducers = combineReducers({
     sideBar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
