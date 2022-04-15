6b29a65af161bef265badd7f99f648d0f6e35113
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2022-04-15 14:02:44 +0300

    redux-form field validation  for login

--- social-network-project/src/components/Login/Login.js
+++ social-network-project/src/components/Login/Login.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { required } from '../../utils/validators';
+import { Input } from '../common/FormsControls/FormsControls';
 
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name ={"login"} component ={"input"}/>
+                <Field placeholder={"Login"} name ={"login"} 
+                    validate ={[required]} component ={Input}/>
             </div>
             <div>
-                <Field placeholder={"Password"} name ={"password"} component ={"input"}/>
+                <Field placeholder={"Password"} name ={"password"} 
+                    validate ={[required]} component ={Input}/>
             </div>
             <div>
-                <Field type={"checkbox"} name ={"rememberMe"} component ={"input"}/> remember me
+                <Field type={"checkbox"} name ={"rememberMe"} 
+                    validate ={[required]} component ={Input}/> remember me
             </div>
             <div>
                 <button>Login</button>
